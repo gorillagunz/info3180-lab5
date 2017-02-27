b6b71eef864fd56e466aed084a2a9874b132c6ad
Jovaun Robinson <jovaunrobinson@yahoo.com>
2017-02-27 02:49:01 +0000

    commit 7-8-9-10

--- app/views.py
+++ app/views.py
@@ -25,16 +25,28 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html')
+@app.route('/secure-page/')
+@login_required
+def secure_page():
+    """Render a secure page."""
+    return render_template('secure_page.html')
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("you have logged out")
+    return redirect(url_for('home'))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" & form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
+            user = UserProfile.query.filter_by(username=username, password=password).first()
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
@@ -42,9 +54,9 @@ def login():
 
             # get user id, load into session
             login_user(user)
-
+            flash('Logged in successfully.')
             # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+            return redirect(url_for("secure_page")) # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
